b6664d49a91a2d7b6ef283dd4503b69f4831639c
Utkarsh-K10 <utkarshkushwah9@gmail.com>
2022-09-01 14:40:48 +0530

    adding NLP application

---
+++ NLPapplication.py
@@ -0,0 +1,64 @@
+raw_docs = ["Here are some very simple basic sentences.",
+"They won't be very interesting, I'm afraid.",
+"The point of these examples is to _learn how basic text cleaning works_ on *very simple* data."]
+
+# Tokenizing text into bags of words
+from nltk.tokenize import word_tokenize
+tokenized_docs = [word_tokenize(doc) for doc in raw_docs]
+print(tokenized_docs)
+
+# Removing punctuation
+import re
+import string
+regex = re.compile('[%s]' % re.escape(string.punctuation)) #see documentation here: http://docs.python.org/2/library/string.html
+
+tokenized_docs_no_punctuation = []
+
+for review in tokenized_docs:
+    new_review = []
+    for token in review:
+        new_token = regex.sub(u'', token)
+        if not new_token == u'':
+            new_review.append(new_token)
+    
+    tokenized_docs_no_punctuation.append(new_review)
+    
+print(tokenized_docs_no_punctuation)
+
+# Cleaning text of stopwords
+from nltk.corpus import stopwords
+
+tokenized_docs_no_stopwords = []
+
+for doc in tokenized_docs_no_punctuation:
+    new_term_vector = []
+    for word in doc:
+        if not word in stopwords.words('english'):
+            new_term_vector.append(word)
+    
+    tokenized_docs_no_stopwords.append(new_term_vector)
+
+print(tokenized_docs_no_stopwords)
+
+# Stemming and Lemmatizing
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+
+porter = PorterStemmer()
+snowball = SnowballStemmer('english')
+wordnet = WordNetLemmatizer()
+
+preprocessed_docs = []
+
+for doc in tokenized_docs_no_stopwords:
+    final_doc = []
+    for word in doc:
+        final_doc.append(porter.stem(word))
+        #final_doc.append(snowball.stem(word))
+        #final_doc.append(wordnet.lemmatize(word))
+    
+    preprocessed_docs.append(final_doc)
+
+print(preprocessed_docs)
+
